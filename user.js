faac47424b5336b9f9444a1d163af319f8376495
kiraoke <kiraokeku@gmail.com>
2025-06-14 23:15:09 +0530

    dont grey out in active windows

--- user.js
+++ user.js
@@ -1421,3 +1421,4 @@ user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("zen.widget.linux.transparency", true);
 user_pref("nebula-tab-loading-animation", 0);
 user_pref("devtools.debugger.remote-enabled", true); // browser dev tools enable
+user_pref("zen.view.grey-out-inactive-windows", false);
